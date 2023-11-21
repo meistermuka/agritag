a80995bd00dfb6eba406f9fe2cf30b0de89647f3
Mukandila Mukandila <mmukandila@intelcom.ca>
2023-11-21 15:22:55 -0500

    Fleshing out Location model

--- AgriTag/Models/Location.cs
+++ AgriTag/Models/Location.cs
@@ -2,5 +2,18 @@
 {
     public class Location
     {
+        public Guid LocationId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public string Address { get; set; }
+        public GeoCoordinates GeoLocation { get; set; }
+        public string PlusCode { get; set; }
+
+    }
+
+    public class GeoCoordinates
+    {
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
     }
 }
