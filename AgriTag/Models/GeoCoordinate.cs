c5b3151b14f409d10c8caafbae624366316838b4
Mukandila Mukandila <ndila@muka.io>
2023-11-21 17:29:03 -0500

    Moved geocoordinates to own class file

---
+++ AgriTag/Models/GeoCoordinate.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AgriTag.Models
+{
+    public class GeoCoordinates
+    {
+        [Key]
+        public Guid GeoCoordinatesId { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+    }
+}
--- AgriTag/Models/Location.cs
+++ AgriTag/Models/Location.cs
@@ -1,19 +1,16 @@
-﻿namespace AgriTag.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AgriTag.Models
 {
     public class Location
     {
+        [Key]
         public Guid LocationId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; } = string.Empty;
         public string Address { get; set; }
-        public GeoCoordinates GeoLocation { get; set; }
-        public string PlusCode { get; set; }
+        public GeoCoordinates? GeoLocation { get; set; }
+        public string PlusCode { get; set; } = string.Empty;
 
-    }
-
-    public class GeoCoordinates
-    {
-        public string Latitude { get; set; }
-        public string Longitude { get; set; }
     }
 }
